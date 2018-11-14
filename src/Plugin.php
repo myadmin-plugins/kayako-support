3df1c868cb3b0e0f9943200e643811ae9b4c90e1
Joe Huss <detain@interserver.net>
2018-11-13 20:30:37 -0500

    localization updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -70,7 +70,7 @@ class Plugin
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
 			if (has_acl('client_billing')) {
-				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', 'Kayako');
+				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', __('Kayako'));
 			}
 		}
 	}
@@ -80,7 +80,10 @@ class Plugin
 	 */
 	public static function getRequirements(GenericEvent $event)
 	{
-		$loader = $event->getSubject();
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
 		$loader->add_requirement('class.Kayako', '/../vendor/detain/myadmin-kayako-support/src/Kayako.php');
 		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/myadmin-kayako-support/src/abuse.inc.php');
 		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/myadmin-kayako-support/src/abuse.inc.php');
@@ -94,11 +97,14 @@ class Plugin
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		$settings = $event->getSubject();
-		$settings->add_text_setting('Support', 'Kayako', 'kayako_api_url', 'Kayako API URL:', 'Kayako API URL', KAYAKO_API_URL);
-		$settings->add_text_setting('Support', 'Kayako', 'kayako_api_key', 'Kayako API Key:', 'Kayako API Key', KAYAKO_API_KEY);
-		$settings->add_text_setting('Support', 'Kayako', 'kayako_api_secret', 'Kayako API Secret:', 'Kayako API Secret', KAYAKO_API_SECRET);
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+		$settings->add_text_setting(__('Support'), __('Kayako'), 'kayako_api_url', __('Kayako API URL'), __('Kayako API URL'), KAYAKO_API_URL);
+		$settings->add_text_setting(__('Support'), __('Kayako'), 'kayako_api_key', __('Kayako API Key'), __('Kayako API Key'), KAYAKO_API_KEY);
+		$settings->add_text_setting(__('Support'), __('Kayako'), 'kayako_api_secret', __('Kayako API Secret'), __('Kayako API Secret'), KAYAKO_API_SECRET);
 	}
 }
